88cf9b1aa33a809683d1205960988cb1d18318b5
Rémi Flamary <remi.flamary@gmail.com>
2016-10-28 17:14:50 +0200

    RTD3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return Mock()
 
-MOCK_MODULES = [ 'emd']
+MOCK_MODULES = [ 'emd','ot.lp.emd']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
