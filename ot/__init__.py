4ced7428bd2be4ca008f12400afb445c5a6517c8
Rémi Flamary <remi.flamary@gmail.com>
2016-10-28 14:19:57 +0200

    relative path

--- ot/__init__.py
+++ ot/__init__.py
@@ -17,6 +17,6 @@ from .bregman import sinkhorn,barycenter
 from .da import sinkhorn_lpl1_mm
 
 # utils functions
-from utils import dist,unif
+from .utils import dist,unif
 
 __all__ = ["emd","sinkhorn","utils",'datasets','bregman','lp','plot','dist','unif','barycenter','sinkhorn_lpl1_mm','da','optim']
