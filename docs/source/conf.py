ecac4ba865460cbb677520a24fc4d2c34442d45a
Rémi Flamary <remi.flamary@gmail.com>
2016-12-02 15:39:33 +0100

    readthedoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,23 +17,23 @@ import os
 import re
 import sphinx_gallery
 
-##try:
-#from unittest.mock import MagicMock
-##except ImportError:
-##    from mock import MagicMock
-#
-#sys.path.insert(0, os.path.abspath("../.."))
-##sys.setrecursionlimit(1500)
-#
-#
-#
-#class Mock(MagicMock):
-#    @classmethod
-#    def __getattr__(cls, name):
-#        return Mock()
-#
-#MOCK_MODULES = [ 'emd','ot.lp.emd']
-#sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+#try:
+from unittest.mock import MagicMock
+#except ImportError:
+#    from mock import MagicMock
+
+sys.path.insert(0, os.path.abspath("../.."))
+#sys.setrecursionlimit(1500)
+
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return Mock()
+
+MOCK_MODULES = [ 'emd','ot.lp.emd']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
