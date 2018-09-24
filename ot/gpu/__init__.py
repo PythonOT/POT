d258c7d6936410cd78189445a0260d983f7684d6
Rémi Flamary <remi.flamary@gmail.com>
2018-09-24 13:57:42 +0200

    convert ot.gpu to cupy

--- ot/gpu/__init__.py
+++ ot/gpu/__init__.py
@@ -4,9 +4,13 @@ from . import bregman
 from . import da
 from .bregman import sinkhorn
 
+from . import utils
+from .utils import dist, to_gpu, to_np
+
+
 # Author: Remi Flamary <remi.flamary@unice.fr>
 #         Leo Gautheron <https://github.com/aje>
 #
 # License: MIT License
 
-__all__ = ["bregman", "da", "sinkhorn"]
+__all__ = ["utils", "dist", "sinkhorn"]
