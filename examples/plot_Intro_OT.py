82e13ec4092d7279049fa79810200e87d93e3355
Aurelie Boisbunon <abb@mydatamodels.com>
2020-06-08 14:18:51 +0200

    add introductory example of OT, EMD and Sinkhorn

---
+++ examples/plot_Intro_OT.py
@@ -0,0 +1,235 @@
+# coding: utf-8
+# -*- coding: utf-8 -*-
+"""
+=============================================
+Introduction to Optimal Transport with Python
+=============================================
+
+This example gives an introduction on how to use Optimal Transport in Python.
+
+"""
+
+# Author: Remi Flamary, Nicolas Courty, Aurelie Boisbunon
+#
+# License: MIT License
+# sphinx_gallery_thumbnail_number = 1
+
+##############################################################################
+# POT installation
+# ----------------
+# 
+# + Install with pip:
+# ```bash
+# pip install pot
+# ```
+# + Install with conda
+# ```bash
+# conda install -c conda-forge pot
+# ```
+
+##############################################################################
+# POT Python Optimal Transport Toolbox
+# ------------------------------------
+# 
+# Import the toolbox
+
+#%% Import libraries
+
+import numpy as np # always need it
+import scipy as sp # often use it
+import pylab as pl # do the plots
+
+import ot # ot 
+
+##############################################################################
+# Getting help
+# 
+# Online  documentation : [https://pythonot.github.io/all.html) 
+# 
+# Or inline help:
+# 
+
+help(ot.dist)
+
+
+##############################################################################
+# First OT Problem
+# ----------------
+# 
+# We will solve the Bakery/Cafés problem of transporting croissants from a number of Bakeries to Cafés in a City (In this case Manhattan). We did a quick google map search in Manhattan for bakeries and Cafés:
+# 
+# ![bak.png](https://remi.flamary.com/cours/otml/bak.png)
+# 
+# We extracted from this search their positions and generated fictional production and sale number (that both sum to the same value).
+# 
+# We have acess to the position of Bakeries ```bakery_pos``` and their respective production ```bakery_prod``` which describe the source distribution. The Cafés where the croissants are sold are defiend also by their position ```cafe_pos``` and ```cafe_prod```. For fun we also provide a map ```Imap``` that will illustrate the position of these shops in the city.
+# 
+# 
+# Now we load the data
+# 
+# 
+
+#%% Load the data
+
+data=np.load('../data/manhattan.npz')
+
+bakery_pos=data['bakery_pos']
+bakery_prod=data['bakery_prod']
+cafe_pos=data['cafe_pos']
+cafe_prod=data['cafe_prod']
+Imap=data['Imap']
+
+print('Bakery production: {}'.format(bakery_prod))
+print('Cafe sale: {}'.format(cafe_prod))
+print('Total croissants : {}'.format(cafe_prod.sum()))
+
+
+##############################################################################
+# Plotting bakeries in the city
+# -----------------------------
+# 
+# Next we plot the position of the bakeries and cafés on the map. The size of the circle is proportional to their production.
+# 
+
+pl.figure(1,(8,7))
+pl.clf()
+pl.imshow(Imap,interpolation='bilinear') # plot the map
+pl.scatter(bakery_pos[:,0],bakery_pos[:,1],s=bakery_prod,c='r', edgecolors='k',label='Bakeries')
+pl.scatter(cafe_pos[:,0],cafe_pos[:,1],s=cafe_prod,c='b', edgecolors='k',label='Cafés')
+pl.legend()
+pl.title('Manhattan Bakeries and Cafés');
+
+#%% Compute cost matrix
+
+##############################################################################
+# Cost matrix
+# -----------
+# 
+# 
+# We compute the cost matrix between the bakeries and the cafés, this will be the transport cost matrix. This can be done using the [ot.dist](https://pythonot.github.io/all.html#ot.dist) that defaults to squared euclidean distance but can return other things such as cityblock (or manhattan distance). 
+# 
+# 
+
+C = ot.dist(bakery_pos, cafe_pos)
+
+labels = [str(i) for i in range(len(bakery_prod))]
+f = pl.figure(2,(8,21), constrained_layout=True)
+pl.clf()
+gs = f.add_gridspec(1, 7)
+f.add_subplot(gs[0, :4])
+pl.imshow(Imap,interpolation='bilinear') # plot the map
+for i in range(len(cafe_pos)):
+    pl.annotate(labels[i], xy=cafe_pos[i,:], color='b', fontsize=14, fontweight='bold', ha='center', va='center')
+for i in range(len(bakery_pos)):
+    pl.text(bakery_pos[i,0], bakery_pos[i,1], labels[i], fontsize=14, color='r', fontweight='bold', ha='center', va='center')
+pl.title('Manhattan Bakeries and Cafés');
+
+ax = f.add_subplot(gs[0, 4:])
+im = pl.imshow(C)
+pl.title('Cost matrix')
+cbar = pl.colorbar(im, ax=ax, shrink=0.15, use_gridspec=True)
+cbar.ax.set_ylabel("cost", rotation=-90, va="bottom")
+
+pl.xlabel('Cafés')
+pl.ylabel('Bakeries')
+pl.show()
+
+
+#%% OT EMD
+
+##############################################################################
+# Solving the OT problem with [ot.emd](https://pythonot.github.io/all.html#ot.emd)
+# -------------
+
+ot_emd = ot.emd(bakery_prod, cafe_prod, C)
+
+# Transportation plan vizualization
+# ---------------------------------
+# 
+# A good vizualization of the OT matrix in the 2D plane is to denote the transportation of mass between a Bakery and a Café by a line. This can easily be done with a double ```for``` loop.
+# 
+# In order to make it more interpretable one can also use the ```alpha``` parameter of plot and set it to ```alpha=G[i,j]/G.max()```. 
+
+f = pl.figure(3,(8,21), constrained_layout=True)
+pl.clf()
+gs = f.add_gridspec(1, 7)
+f.add_subplot(gs[0, :4])
+pl.imshow(Imap,interpolation='bilinear') # plot the map
+for i in range(len(bakery_pos)):
+    for j in range(len(cafe_pos)):
+        pl.plot([bakery_pos[i,0],cafe_pos[j,0]], [bakery_pos[i,1],cafe_pos[j,1]], '-k', lw=3.*ot_emd[i,j]/ot_emd.max())
+for i in range(len(cafe_pos)):
+    pl.annotate(labels[i], xy=cafe_pos[i,:], color='b', fontsize=14, fontweight='bold', ha='center', va='center')
+for i in range(len(bakery_pos)):
+    pl.text(bakery_pos[i,0], bakery_pos[i,1], labels[i], fontsize=14, color='r', fontweight='bold', ha='center', va='center')
+pl.title('Manhattan Bakeries and Cafés');
+
+ax = f.add_subplot(gs[0, 4:])
+im = pl.imshow(ot_emd)
+pl.title('Transport matrix')
+cbar = f.colorbar(im, ax=ax, shrink=0.15, use_gridspec=True)
+cbar.ax.set_ylabel("transport", rotation=-90, va="bottom")
+
+pl.xlabel('Cafés')
+pl.ylabel('Bakeries')
+pl.show()
+
+#%% Wasserstein loss
+
+##############################################################################
+# OT loss and dual variables
+# --------------------------
+# 
+# The resulting wasserstein loss loss is of the form:
+# 
+# $W=\sum_{i,j}\gamma_{i,j}C_{i,j}$
+# 
+# where $\gamma$ is the optimal transport matrix.
+# 
+
+W = np.sum(ot_emd*C)
+print('Wasserstein loss = {0:.3f}'.format(W))
+
+#%% OT Sinkhorn
+
+##############################################################################
+# Regularized OT with Sinkhorn
+# ----------------------------
+# 
+# The Sinkhorn algorithm is very simple to code. You can implement it directly using the following pseudo-code:
+# 
+# ![sinkhorn.png](attachment:sinkhorn.png)
+# 
+# An alternative is to use the POT toolbox with [ot.sinkhorn](https://pythonot.github.io/all.html#ot.sinkhorn)
+# 
+# Be carefull to numerical problems. A good pre-processing for Sinkhorn is to divide the cost matrix ```C```
+#  by its maximum value.
+
+ot_sinkhorn = ot.sinkhorn(bakery_prod, cafe_prod, reg=0.1, M=C/C.max())
+f = pl.figure(4,(8,21), constrained_layout=True)
+pl.clf()
+gs = f.add_gridspec(1, 7)
+f.add_subplot(gs[0, :4])
+pl.imshow(Imap,interpolation='bilinear') # plot the map
+for i in range(len(bakery_pos)):
+    for j in range(len(cafe_pos)):
+        pl.plot([bakery_pos[i,0],cafe_pos[j,0]], [bakery_pos[i,1],cafe_pos[j,1]], '-k', lw=3.*ot_sinkhorn[i,j]/ot_sinkhorn.max())
+for i in range(len(cafe_pos)):
+    pl.annotate(labels[i], xy=cafe_pos[i,:], color='b', fontsize=14, fontweight='bold', ha='center', va='center')
+for i in range(len(bakery_pos)):
+    pl.text(bakery_pos[i,0], bakery_pos[i,1], labels[i], fontsize=14, color='r', fontweight='bold', ha='center', va='center')
+pl.title('Manhattan Bakeries and Cafés');
+
+ax = f.add_subplot(gs[0, 4:])
+im = pl.imshow(ot_sinkhorn)
+pl.title('Transport matrix')
+cbar = f.colorbar(im, ax=ax, shrink=0.15, use_gridspec=True)
+cbar.ax.set_ylabel("transport", rotation=-90, va="bottom")
+
+pl.xlabel('Cafés')
+pl.ylabel('Bakeries')
+pl.show()
+
+W_sinkhorn = np.sum(ot_sinkhorn*C)
+print('Wasserstein loss (EMD) = {0:.3f}'.format(W))
+print('Wasserstein loss (Sink) = {0:.3f}'.format(W_sinkhorn))
