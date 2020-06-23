f077858973fbdc8483411ebb6a3490733ce00267
Aurelie Boisbunon <abb@mydatamodels.com>
2020-06-23 10:53:06 +0200

    add subsections and modify figure sizes/shapes

--- examples/plot_Intro_OT.py
+++ examples/plot_Intro_OT.py
@@ -99,7 +99,7 @@ print('Total croissants : {}'.format(cafe_prod.sum()))
 # the circle is proportional to their production.
 #
 
-pl.figure(1, (8, 7))
+pl.figure(1, (7, 6))
 pl.clf()
 pl.imshow(Imap, interpolation='bilinear')  # plot the map
 pl.scatter(bakery_pos[:, 0], bakery_pos[:, 1], s=bakery_prod, c='r', ec='k', label='Bakeries')
@@ -124,7 +124,7 @@ pl.title('Manhattan Bakeries and Cafés')
 C = ot.dist(bakery_pos, cafe_pos)
 
 labels = [str(i) for i in range(len(bakery_prod))]
-f = pl.figure(2, (13, 6))
+f = pl.figure(2, (14, 7))
 pl.clf()
 pl.subplot(121)
 pl.imshow(Imap, interpolation='bilinear')  # plot the map
@@ -177,7 +177,7 @@ time_emd = time.time() - start
 # parameter of plot and set it to ``alpha=G[i,j]/G.max()``.
 
 # Plot the matrix and the map
-f = pl.figure(3, (13, 6))
+f = pl.figure(3, (14, 7))
 pl.clf()
 pl.subplot(121)
 pl.imshow(Imap, interpolation='bilinear')  # plot the map
@@ -247,6 +247,10 @@ print('Wasserstein loss = {0:.3f}'.format(W))
 # Be carefull to numerical problems. A good pre-processing for Sinkhorn is to
 # divide the cost matrix ``C`` by its maximum value.
 
+##############################################################################
+# Algorithm
+# `````````
+
 # Compute Sinkhorn transport matrix from algorithm
 reg = 0.1
 K = np.exp(-C / C.max() / reg)
@@ -261,9 +265,14 @@ ot_sink_algo = np.atleast_2d(u).T * (K * v.T)  # Equivalent to np.dot(np.diag(u)
 ot_sinkhorn = ot.sinkhorn(bakery_prod, cafe_prod, reg=reg, M=C / C.max())
 
 # Difference between the 2
-print(np.sum(np.power(ot_sink_algo - ot_sinkhorn, 2)))
+print('Difference between algo and ot.sinkhorn = {0:.2d}'.format(np.sum(np.power(ot_sink_algo - ot_sinkhorn, 2))))
 
+##############################################################################
 # Plot the matrix and the map
+# ```````````````````````````
+
+print('Min. of Sinkhorn\'s transport matrix = {0:g}'.format(np.min(ot_sinkhorn)))
+
 f = pl.figure(4, (13, 6))
 pl.clf()
 pl.subplot(121)
@@ -293,7 +302,6 @@ pl.xlabel('Cafés')
 pl.ylabel('Bakeries')
 pl.show()
 
-print(np.min(ot_sinkhorn))
 
 ##############################################################################
 # We notice right away that the matrix is not sparse at all with Sinkhorn,
@@ -310,16 +318,16 @@ reg_parameter = np.logspace(-3, 0, 20)
 W_sinkhorn_reg = np.zeros((len(reg_parameter), ))
 time_sinkhorn_reg = np.zeros((len(reg_parameter), ))
 
-f = pl.figure(5, (8, 10))
+f = pl.figure(5, (14, 5))
 pl.clf()
-max_ot = 100  # plot all matrices with the same colorbar
+max_ot = 100  # plot matrices with the same colorbar
 for k in range(len(reg_parameter)):
     start = time.time()
     ot_sinkhorn = ot.sinkhorn(bakery_prod, cafe_prod, reg=reg_parameter[k], M=C / C.max())
     time_sinkhorn_reg[k] = time.time() - start
 
-    if k % 2 == 0 and k > 0:  # we only plot one out of 2
-        ax = pl.subplot(3, 3, k / 2)
+    if k % 3 == 0 and k > 0:  # we only plot a few
+        ax = pl.subplot(1, 6, k / 3)
         im = pl.imshow(ot_sinkhorn, vmin=0, vmax=max_ot)
         pl.title('reg={0:.2g}'.format(reg_parameter[k]))
         pl.xlabel('Cafés')
@@ -335,6 +343,11 @@ pl.tight_layout()
 # very similar to EMD (although not sparse) for very small values of the
 # regularization parameter, and tends to a more uniform solution as the
 # regularization parameter increases.
+#
+
+##############################################################################
+# Wasserstein loss and computational time
+# ```````````````````````````````````````
 #
 
 # Plot the matrix and the map
