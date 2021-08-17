cbcb57e8ae2fceb3fa9681f3e949c8ca5316b4cb
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2021-08-17 10:36:10 +0200

    we don't test with 3.5 but we already do with 3.9 so setup was not up to date

--- setup.py
+++ setup.py
@@ -81,9 +81,9 @@ setup(
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ]
 )
