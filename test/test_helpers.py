dc9a47548a5012ccf25bb7450a65aa6db6686d4e
ncassereau <nathan.cassereau@idris.fr>
2021-09-10 11:19:07 +0200

    Test helper functions

---
+++ test/test_helpers.py
@@ -0,0 +1,26 @@
+"""Tests for backend module """
+
+# Author: Remi Flamary <remi.flamary@polytechnique.edu>
+#
+# License: MIT License
+
+import os
+import sys
+
+sys.path.append(os.path.join("ot", "helpers"))
+
+from openmp_helpers import get_openmp_flag, check_openmp_support # noqa
+from pre_build_helpers import _get_compiler, compile_test_program # noqa
+
+
+def test_helpers():
+
+    compiler = _get_compiler()
+
+    get_openmp_flag(compiler)
+
+    s = '#include <stdio.h>\n#include <stdlib.h>\n\nint main(void) {\n\tprintf("Hello world!\\n");\n\treturn 0;\n}'
+    output = compile_test_program(s)
+    assert len(output) == 1 and "Hello world!" == output[0]
+
+    check_openmp_support()
