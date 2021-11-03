5ad21abb5ac22b38c889c40105566a43800c24b3
ncassereau <nathan.cassereau@idris.fr>
2021-11-03 15:56:14 +0100

    first try

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -81,8 +81,8 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+#source_suffix = '.rst'
 
 # The encoding of source files.
 source_encoding = 'utf-8-sig'
