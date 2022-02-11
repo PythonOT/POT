aa1a4950bac7769cbfee60740ca4676021d4f221
David Ghiurco <9147386+davidghiurco@users.noreply.github.com>
2022-02-11 11:50:26 -0600

    Set minimum Python requirement to `>=3.7` in setup.py since !328 removed Python 3.6 support - build all wheels

--- setup.py
+++ setup.py
@@ -70,6 +70,7 @@ setup(
     data_files=[],
     setup_requires=["oldest-supported-numpy", "cython>=0.23"],
     install_requires=["numpy>=1.20", "scipy>=1.0"],
+    python_requires=">=3.7",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -91,7 +92,6 @@ setup(
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
