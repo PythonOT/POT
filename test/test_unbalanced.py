97282df7e7a8512e5bb27a2d93b2c8e845feb985
Laetitia Chapel <laetitia.chapel@univ-ubs.fr>
2022-04-11 11:19:05 +0200

    add test mm algo unbalanced OT

--- test/test_unbalanced.py
+++ test/test_unbalanced.py
@@ -321,11 +321,11 @@ def test_mm_convergence(nx):
                                atol=1e-5)
 
     # check in case no histogram is provided
-    M_np = nx.to_numpy(M)
+    M = nx.to_numpy(M)
     a_np, b_np = np.array([]), np.array([])
     a, b = nx.from_numpy(a_np, b_np)
 
-    G_kl_null, _ = ot.unbalanced.mm_unbalanced(a, b, M, reg_m=reg_m, div='kl')
+    G_kl_null, _ = ot.unbalanced.mm_unbalanced(a, b, M_, reg_m=reg_m, div='kl')
     G_l2_null, _ = ot.unbalanced.mm_unbalanced(a, b, M, reg_m=reg_m, div='l2')
     np.testing.assert_allclose(G_kl_null, G_kl)
     np.testing.assert_allclose(G_l2_null, G_l2)
