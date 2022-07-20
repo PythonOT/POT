5e6b95fbf03a63a617b5c0da7d3bd31fe22593fc
Rémi Flamary <remi.flamary@gmail.com>
2022-07-20 13:37:13 +0200

    add test for more complex attibutes

--- test/test_solvers.py
+++ test/test_solvers.py
@@ -55,10 +55,10 @@ def test_solve(nx):
     sol = ot.solve(M, a, b)
 
     # check some attributes
-    potentials = sol.potentials
-    sparse_plan = sol.sparse_plan
-    marginals = sol.marginals
-    status = sol.status
+    sol.potentials
+    sol.sparse_plan
+    sol.marginals
+    sol.status
 
     assert_allclose_sol(sol0, sol)
 
