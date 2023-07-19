6a2149f6e6aa3054c51f3753e332d45766e50792
Antoine Collas <22830806+antoinecollas@users.noreply.github.com>
2023-07-19 11:30:16 +0200

    improve EWCA example

--- examples/others/plot_EWCA.py
+++ examples/others/plot_EWCA.py
@@ -15,13 +15,12 @@ Entropic Wasserstein Component Analysis.
 # Author: Antoine Collas <antoine.collas@inria.fr>
 #
 # License: MIT License
-# sphinx_gallery_thumbnail_number = 3
+# sphinx_gallery_thumbnail_number = 2
 
 import numpy as np
 import matplotlib.pylab as pl
 from ot.dr import ewca
 from sklearn.datasets import make_blobs
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 from matplotlib import ticker as mticker
 import matplotlib.patches as patches
 import matplotlib
@@ -47,59 +46,81 @@ X, y = make_blobs(
 X = X - X.mean(0)
 
 ##############################################################################
+# Plot data
+# -------------
+
+fig = pl.figure(figsize=(4, 4))
+cmap = matplotlib.colormaps.get_cmap("tab10")
+pl.scatter(
+    X[: n_samples // 2, 0],
+    X[: n_samples // 2, 1],
+    color=[cmap(y[i] + 1) for i in range(n_samples // 2)],
+    alpha=0.4,
+    label="Class 1",
+    zorder=30,
+    s=50,
+)
+pl.scatter(
+    X[n_samples // 2:, 0],
+    X[n_samples // 2:, 1],
+    color=[cmap(y[i] + 1) for i in range(n_samples // 2, n_samples)],
+    alpha=0.4,
+    label="Class 2",
+    zorder=30,
+    s=50,
+)
+x_y_lim = 2.5
+fs = 15
+pl.xlim(-x_y_lim, x_y_lim)
+pl.xticks([])
+pl.ylim(-x_y_lim, x_y_lim)
+pl.yticks([])
+pl.legend(fontsize=fs)
+pl.title("Data", fontsize=fs)
+pl.tight_layout()
+
+
+##############################################################################
 # Compute EWCA
 # -------------
+
 pi, U = ewca(X, k=2, reg=0.5)
 
 
 ##############################################################################
-# Plot
+# Plot data, first component, and projected data
 # -------------
-fs = 15
+
+fig = pl.figure(figsize=(4, 4))
+
 scale = 3
-origin = np.array([0, 0])
-cmap = matplotlib.colormaps.get_cmap("tab10")
-
-fig, ax = pl.subplots(1, 2, figsize=(10, 5))
 u = U[:, 0]
-ax[0].plot(
-    [origin[0], scale * u[0] + origin[0]],
-    [origin[1], scale * u[1] + origin[1]],
+pl.plot(
+    [scale * u[0], -scale * u[0]],
+    [scale * u[1], -scale * u[1]],
     color="grey",
     linestyle="--",
     lw=3,
     alpha=0.3,
+    label=r"$\mathbf{U}$",
 )
-label_ = r"$\mathbf{U}$"
-ax[0].plot(
-    [origin[0], -scale * u[0] + origin[0]],
-    [origin[1], -scale * u[1] + origin[1]],
-    color="grey",
-    linestyle="--",
-    lw=3,
-    alpha=0.3,
-    label=label_,
-)
-X1 = X @ u[:, None] @ u[:, None].T + origin
+X1 = X @ u[:, None] @ u[:, None].T
 
-ax[0].axis("scaled")
-thresh = 0.15
-mm = 1
 for i in range(n_samples):
     for j in range(n_samples):
         v = pi[i, j] / pi.max()
-        if v >= thresh or (i, j) == (n_samples - 1, n_samples - 1):
-            ax[0].plot(
+        if v >= 0.15 or (i, j) == (n_samples - 1, n_samples - 1):
+            pl.plot(
                 [X[i, 0], X1[j, 0]],
                 [X[i, 1], X1[j, 1]],
-                alpha=mm * v,
+                alpha=v,
                 linestyle="-",
                 c="C0",
                 label=r"$\pi_{ij}$"
                 if (i, j) == (n_samples - 1, n_samples - 1)
                 else None,
             )
-ax[0].scatter(
+pl.scatter(
     X[:, 0],
     X[:, 1],
     color=[cmap(y[i] + 1) for i in range(n_samples)],
@@ -108,7 +129,7 @@ ax[0].scatter(
     zorder=30,
     s=50,
 )
-ax[0].scatter(
+pl.scatter(
     X1[:, 0],
     X1[:, 1],
     color=[cmap(y[i] + 1) for i in range(n_samples)],
@@ -118,15 +139,24 @@ ax[0].scatter(
     label=r"$\mathbf{U}\mathbf{U}^{\top}\mathbf{x}_i$",
     zorder=30,
 )
-ax[0].set_xticks([])
-ax[0].set_yticks([])
-ax[0].legend(fontsize=fs, loc="upper left")
+pl.title("Data and projections", fontsize=fs)
+pl.xlim(-x_y_lim, x_y_lim)
+pl.xticks([])
+pl.ylim(-x_y_lim, x_y_lim)
+pl.yticks([])
+pl.legend(fontsize=fs, loc="upper left")
+pl.tight_layout()
 
-divider = make_axes_locatable(ax[1])
+
+##############################################################################
+# Plot transport plan
+# -------------
+
+fig = pl.figure(figsize=(5, 5))
+
 norm = matplotlib.colors.PowerNorm(0.5, vmin=0, vmax=100)
-im = ax[1].imshow(n_samples * pi * 100, cmap=pl.cm.Blues, norm=norm, aspect="auto")
-cax = divider.append_axes("right", size="5%", pad=0.05)
-cb = fig.colorbar(im, cax=cax, orientation="vertical")
+im = pl.imshow(n_samples * pi * 100, cmap=pl.cm.Blues, norm=norm, aspect="auto")
+cb = fig.colorbar(im, orientation="vertical", shrink=0.8)
 ticks_loc = cb.ax.get_yticks().tolist()
 cb.ax.yaxis.set_major_locator(mticker.FixedLocator(ticks_loc))
 cb.ax.set_yticklabels([f"{int(i)}%" for i in cb.get_ticks()])
@@ -144,13 +174,14 @@ for i, class_ in enumerate(np.sort(np.unique(y))):
         edgecolor="r",
         facecolor="none",
     )
-    ax[1].add_patch(rect)
+    pl.gca().add_patch(rect)
 
-ax[1].set_title("OT plan", fontsize=fs)
-ax[1].set_ylabel(r"($\mathbf{x}_1, \cdots, \mathbf{x}_n$)")
+pl.title("OT plan", fontsize=fs)
+pl.ylabel(r"($\mathbf{x}_1, \cdots, \mathbf{x}_n$)")
 x_label = r"($\mathbf{U}\mathbf{U}^{\top}\mathbf{x}_1, \cdots,"
 x_label += r"\mathbf{U}\mathbf{U}^{\top}\mathbf{x}_n$)"
-ax[1].set_xlabel(x_label)
-
+pl.xlabel(x_label)
 pl.tight_layout()
+pl.axis("scaled")
+
 pl.show()
