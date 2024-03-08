7d3071f1f31ac03ca7c3f9d766a169b381562279
laudavid <laurene.gdavid@gmail.com>
2024-03-08 15:29:52 +0100

    Implementation of LR GW + add method in __init__

--- ot/__init__.py
+++ ot/__init__.py
@@ -53,7 +53,7 @@ from .gromov import (gromov_wasserstein, gromov_wasserstein2,
 from .weak import weak_optimal_transport
 from .factored import factored_optimal_transport
 from .solvers import solve, solve_gromov, solve_sample
-from .lowrank import lowrank_sinkhorn
+from .lowrank import lowrank_sinkhorn, lowrank_gromov_wasserstein
 
 # utils functions
 from .utils import dist, unif, tic, toc, toq
@@ -71,4 +71,5 @@ __all__ = ['emd', 'emd2', 'emd_1d', 'sinkhorn', 'sinkhorn2', 'utils',
            'factored_optimal_transport', 'solve', 'solve_gromov','solve_sample', 
            'smooth', 'stochastic', 'unbalanced', 'partial', 'regpath', 'solvers',
            'binary_search_circle', 'wasserstein_circle',
-           'semidiscrete_wasserstein2_unif_circle', 'sliced_wasserstein_sphere_unif', 'lowrank_sinkhorn']
+           'semidiscrete_wasserstein2_unif_circle', 'sliced_wasserstein_sphere_unif', 'lowrank_sinkhorn', 
+           'lowrank_gromov_wasserstein']
