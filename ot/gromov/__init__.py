2472dd462a86153da5e3864be1c102ac4046455b
Laurène <92529964+laudavid@users.noreply.github.com>
2024-05-29 15:22:39 +0200

    Implementation of Low Rank Gromov-Wasserstein (#614)
    * new file for lr sinkhorn
    
    * lr sinkhorn, solve_sample, OTResultLazy
    
    * add test functions + small modif lr_sin/solve_sample
    
    * add import to __init__
    
    * modify low rank, remove solve_sample,OTResultLazy
    
    * new file for lr sinkhorn
    
    * lr sinkhorn, solve_sample, OTResultLazy
    
    * add test functions + small modif lr_sin/solve_sample
    
    * add import to __init__
    
    * remove test solve_sample
    
    * add value, value_linear, lazy_plan
    
    * add comments to lr algorithm
    
    * modify test functions + add comments to lowrank
    
    * modify __init__ with lowrank
    
    * debug lowrank + test
    
    * debug test function low_rank
    
    * error test
    
    * final debug of lowrank + add new test functions
    
    * Debug tests + add lowrank to solve_sample
    
    * fix torch backend for lowrank
    
    * fix jax backend and skip tf
    
    * fix pep 8 tests
    
    * add lowrank init + test functions
    
    * Add init strategies in lowrank + example (PythonOT#588)
    
    * modified lowrank
    
    * changes from code review
    
    * fix error test pep8
    
    * fix linux-minimal-deps + code review
    
    * Implementation of LR GW + add method in __init__
    
    * add LR gw paper in README.md
    
    * add tests for low rank GW
    
    * add examples for Low Rank GW
    
    * fix __init__
    
    * change atol of lr backends
    
    * fix pep8 errors
    
    * modif for code review

--- ot/gromov/__init__.py
+++ ot/gromov/__init__.py
@@ -47,6 +47,8 @@ from ._dictionary import (gromov_wasserstein_dictionary_learning,
                           fused_gromov_wasserstein_dictionary_learning,
                           fused_gromov_wasserstein_linear_unmixing)
 
+from ._lowrank import (_flat_product_operator, lowrank_gromov_wasserstein_samples)
+
 
 __all__ = ['init_matrix', 'tensor_product', 'gwloss', 'gwggrad', 'update_square_loss',
            'update_kl_loss', 'update_feature_matrix', 'init_matrix_semirelaxed',
@@ -64,4 +66,4 @@ __all__ = ['init_matrix', 'tensor_product', 'gwloss', 'gwggrad', 'update_square_
            'entropic_semirelaxed_gromov_wasserstein2', 'entropic_semirelaxed_fused_gromov_wasserstein',
            'entropic_semirelaxed_fused_gromov_wasserstein2', 'gromov_wasserstein_dictionary_learning',
            'gromov_wasserstein_linear_unmixing', 'fused_gromov_wasserstein_dictionary_learning',
-           'fused_gromov_wasserstein_linear_unmixing']
+           'fused_gromov_wasserstein_linear_unmixing', 'lowrank_gromov_wasserstein_samples']
