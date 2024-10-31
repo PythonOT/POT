b70b968cf301de254bab181cd8cf0c4f1ea05692
Titouan Vayer <titouan.vayer@gmail.com>
2024-10-31 11:56:05 +0100

    Create _approx_kernel.py

---
+++ ot/bregman/_approx_kernel.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Approximation of the Gram matrix for entropic regularized OT
+"""
+
+# Author: Titouan Vayer <titouan.vayer@irisa.fr>
+#         Remi Flamary <remi.flamary@unice.fr>
+#
+# License: MIT License
