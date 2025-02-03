f33062a62efaf9748ef9ee843e7bf40676e05a8d
fegounna <yessin.moakher@polytechnique.edu>
2025-02-03 19:40:01 +0100

    implementation of low rank ot via factor relaxation paper

---
+++ ot/low_rank/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Low rank Solvers
+"""
+
+# Author: Yessin Moakher <yessin.moakher@polytechnique.edu>
+#
+# License: MIT License
+
+from ._factor_relaxation import solve_balanced_FRLC
+
+__all__ = ["solve_balanced_FRLC"]
