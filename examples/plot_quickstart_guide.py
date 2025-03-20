67b33e713faa2b5e9f609203da8bc48ea745314c
Rémi Flamary <remi.flamary@gmail.com>
2025-03-20 18:06:39 +0100

    rrme to user guide and add quicksrat script

---
+++ examples/plot_quickstart_guide.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+"""
+=============================================
+Quickstart Guide
+=============================================
+
+
+This is a quickstart guide to the Python Optimal Transport (POT) toolbox.
+
+"""
+
+# Author: Remi Flamary
+#
+# License: MIT License
+# sphinx_gallery_thumbnail_number = 1
+
+# %%
+# Simple example
+# --------------
+#
+
+import numpy as np  # always need it
+import pylab as pl  # for the plots
+
+import ot
