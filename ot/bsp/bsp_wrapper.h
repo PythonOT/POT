da9cc30cd68e5ccee93e589ed1c2224cc1cc64e5
Rémi Flamary <remi.flamary@gmail.com>
2025-11-18 15:15:54 +0100

    fix h file

--- ot/bsp/bsp_wrapper.h
+++ ot/bsp/bsp_wrapper.h
@@ -4,6 +4,6 @@
 #include <cstdint>
 
 
-double BSPOT_wrap(int n1, int n2, int d, double *X, double *Y, uint64_t nb_plans, int *plans, int *plan)
+double BSPOT_wrap(int n1, int n2, int d, double *X, double *Y, uint64_t nb_plans, int *plans, int *plan);
 
 #endif
