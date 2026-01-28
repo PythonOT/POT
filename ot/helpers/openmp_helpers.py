7e4962a80ec85c1961ac6354a6daf8d2daf8b3d7
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2026-01-28 15:54:29 +0100

    Detecting openmp flags on macOS (with libomp from brew) --lint

--- ot/helpers/openmp_helpers.py
+++ ot/helpers/openmp_helpers.py
@@ -40,7 +40,7 @@ def get_openmp_flag(compiler):
                 omp_flag += ["-I/opt/homebrew/opt/libomp/include"]
                 omp_flag += ["/opt/homebrew/opt/libomp/lib/libomp.dylib"]
             else:
-                # Assuming `brew install libomp` on old macOS version
+                # Assuming `brew install libomp` on old macOS
                 if os.path.isfile("/usr/local/opt/libomp/lib/libomp.dylib"):
                     omp_flag += ["-I/usr/local/opt/libomp/include"]
                     omp_flag += ["/usr/local/opt/libomp/lib/libomp.dylib"]
