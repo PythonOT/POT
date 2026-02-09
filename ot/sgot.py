9b49c768b1a0415ff6a020cbb9eeaf338056e536
thibaut-germain <thibaut.germain.pro@gmail.com>
2026-02-09 15:05:52 +0100

    add sgot file

---
+++ ot/sgot.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Optimal transport for linear operators.
+"""
+
+# Author: Sienna O'Shea  <osheasienna@gmail.com>
+#         Thibaut Germain<thibaut.germain.pro@gmail.com>
+# License: MIT License
