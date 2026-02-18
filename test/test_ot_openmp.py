a50746aebe8177f705bf155ee6e57e3d180948c7
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2026-02-18 15:36:26 +0100

    Reverting openmp compilation flags (ruff-format)

--- test/test_ot_openmp.py
+++ test/test_ot_openmp.py
@@ -1,6 +1,7 @@
 import ot
 import numpy as np
 
+
 def test_omp_emd2():
     # test emd2 and emd2 with openmp for simple identity
     n = 1000
@@ -14,5 +15,5 @@ def test_omp_emd2():
     w = ot.emd2(u, u, M)
     w2 = ot.emd2(u, u, M, numThreads=8)
 
-test_omp_emd2()
 
+test_omp_emd2()
